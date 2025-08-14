21ed1730cbee98683578a9e929370f96f3376003
golftaweerak <taweerak.t@promma.ac.th>
2025-08-15 02:41:23 +0700

    drop fown
    Refactor code structure for improved readability and maintainability

--- data/quizzes-list.js
+++ data/quizzes-list.js
@@ -1,50 +1,65 @@
 const quizList = [
-    {
-        title: "ดาราศาสตร์ ม.ต้น ชุด 1 (120 ข้อ)",
-        description: "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
-        url: "./Junior/junior1.html",
-        icon: "./astronomy1.png",
-        borderColor: "border-pink-500",
-        altText: "ไอคอนดาราศาสตร์ระดับมัธยมต้น"
-    },
-    {
-        title: "ดาราศาสตร์ ม.ต้น ชุด 2 (120 ข้อ)",
-        description: "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
-        url: "./Junior/junior2.html",
-        icon: "./astronomy1.png",
-        borderColor: "border-pink-500",
-        altText: "ไอคอนดาราศาสตร์ระดับมัธยมต้น"
-    },
-    {
-        title: "ดาราศาสตร์ ม.ปลาย ชุด 1 (120 ข้อ)",
-        description: "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
-        url: "./Senior/senior1.html",
-        icon: "./astronomy.png",
-        borderColor: "border-blue-500",
-        altText: "ไอคอนดาราศาสตร์ระดับมัธยมปลาย"
-    },
-    {
-        title: "ดาราศาสตร์ ม.ปลาย ชุด 2 (120 ข้อ)",
-        description: "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
-        url: "./Senior/senior2.html",
-        icon: "./astronomy.png",
-        borderColor: "border-blue-500",
-        altText: "ไอคอนดาราศาสตร์ระดับมัธยมปลาย"
-    },
-    {
-        title: "วิทยาศาสตร์โลกและอวกาศ ชุด 1 (150 ข้อ)",
-        description: "แบบทดสอบครอบคลุมเนื้อหา ธรณีวิทยา อุตุนิยมวิทยา และดาราศาสตร์",
-        url: "./EarthScience/ES1.html",
-        icon: "./geology.png",
-        borderColor: "border-green-500",
-        altText: "ไอคอนวิทยาศาสตร์โลกและอวกาศ"
-    },
-    {
-        title: "วิทยาศาสตร์โลกและอวกาศ ชุด 2 (150 ข้อ)",
-        description: "แบบทดสอบครอบคลุมเนื้อหา ธรณีวิทยา อุตุนิยมวิทยา และดาราศาสตร์",
-        url: "./EarthScience/ES2.html",
-        icon: "./geology.png",
-        borderColor: "border-green-500",
-        altText: "ไอคอนวิทยาศาสตร์โลกและอวกาศ"
-    }
+  {
+    title: "ดาราศาสตร์ รวม ชุด 1 (50 ข้อ)",
+    description: "แบบฝึกหัด ทบทวนความเข้าใจทางดาราศาสตร์",
+    url: "./Astronomy/astro1.html",
+    icon: "./space.png",
+    borderColor: "border-red-500",
+    altText: "ไอคอนดาราศาสตร์",
+  },
+  {
+    title: "ดาราศาสตร์ ม.ต้น ชุด 1 (120 ข้อ)",
+    description:
+      "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
+    url: "./Junior/junior1.html",
+    icon: "./astronomy1.png",
+    borderColor: "border-pink-500",
+    altText: "ไอคอนดาราศาสตร์ระดับมัธยมต้น",
+  },
+  {
+    title: "ดาราศาสตร์ ม.ต้น ชุด 2 (120 ข้อ)",
+    description:
+      "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
+    url: "./Junior/junior2.html",
+    icon: "./astronomy1.png",
+    borderColor: "border-pink-500",
+    altText: "ไอคอนดาราศาสตร์ระดับมัธยมต้น",
+  },
+  {
+    title: "ดาราศาสตร์ ม.ปลาย ชุด 1 (120 ข้อ)",
+    description:
+      "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
+    url: "./Senior/senior1.html",
+    icon: "./astronomy.png",
+    borderColor: "border-blue-500",
+    altText: "ไอคอนดาราศาสตร์ระดับมัธยมปลาย",
+  },
+  {
+    title: "ดาราศาสตร์ ม.ปลาย ชุด 2 (120 ข้อ)",
+    description:
+      "แบบทดสอบความเข้าใจพื้นฐานทางดาราศาสตร์และฟิสิกส์ที่เกี่ยวข้อง",
+    url: "./Senior/senior2.html",
+    icon: "./astronomy.png",
+    borderColor: "border-blue-500",
+    altText: "ไอคอนดาราศาสตร์ระดับมัธยมปลาย",
+  },
+
+  {
+    title: "วิทยาศาสตร์โลกและอวกาศ ชุด 1 (150 ข้อ)",
+    description:
+      "แบบทดสอบครอบคลุมเนื้อหา ธรณีวิทยา อุตุนิยมวิทยา และดาราศาสตร์",
+    url: "./EarthScience/ES1.html",
+    icon: "./geology.png",
+    borderColor: "border-green-500",
+    altText: "ไอคอนวิทยาศาสตร์โลกและอวกาศ",
+  },
+  {
+    title: "วิทยาศาสตร์โลกและอวกาศ ชุด 2 (150 ข้อ)",
+    description:
+      "แบบทดสอบครอบคลุมเนื้อหา ธรณีวิทยา อุตุนิยมวิทยา และดาราศาสตร์",
+    url: "./EarthScience/ES2.html",
+    icon: "./geology.png",
+    borderColor: "border-green-500",
+    altText: "ไอคอนวิทยาศาสตร์โลกและอวกาศ",
+  },
 ];
