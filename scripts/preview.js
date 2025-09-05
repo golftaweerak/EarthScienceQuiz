450dcaa712184971ae40ce768614aa43af1d81a1
golftaweerak <taweerak.t@promma.ac.th>
2025-09-05 16:11:26 +0700

    Update preview.js

--- scripts/preview.js
+++ scripts/preview.js
@@ -450,8 +450,8 @@ function renderQuizData() {
                 delimiters: [
                     { left: '$$', right: '$$', display: true },
                     { left: '$', right: '$', display: false },
-                    { left: '\(', right: '\)', display: false },
-                    { left: '\\\\[', right: '\\\\\]', display: true }
+                    { left: '\\(', right: '\\)', display: false },
+                    { left: '\\[', right: '\\]', display: true }
                 ],
                 throwOnError: false
             });
